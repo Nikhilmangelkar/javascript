81dd7446062abe0a47adc10f1f04cdd6456814df
Nikhil Mangelkar <161593349+Nikhilmangelkar@users.noreply.github.com>
2025-03-30 08:16:12 +0000

    arrays

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,13 @@
+const myArr=[0,1,2,3,4,5]
+
+const myHeroes=["iornman","thor"]
+
+const myArr2=new Array(1,2,3,4)
+
+console.log(myArr[0]);
+
+
+//array methods
+
+myArr.push(6)
+console.log(myArr);
