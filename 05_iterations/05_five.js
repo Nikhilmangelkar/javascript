360e25077215e70da0f8f27aefa06e4e8e0c580b
Nikhil Mangelkar <161593349+Nikhilmangelkar@users.noreply.github.com>
2025-03-31 08:08:13 +0000

    reduce

--- 05_iterations/05_five.js
+++ 05_iterations/05_five.js
@@ -1,10 +1,39 @@
-const nums=[1,2,3,4,5,6]
+const nums=[1,2,3]
 
 // const newnums=nums.map((num)=>{return num+10})
 // console.log(newnums);
 
-const newNums=nums.map((num)=>num*10)
-                  .map((num)=>num+1)
-                  .filter((num)=>num>=40)
+// const newNums=nums.map((num)=>num*10)
+//                   .map((num)=>num+1)
+//                   .filter((num)=>num>=40)
 
-    console.log(newNums)              
+//     console.log(newNums)  
+    
+// const myTotal=nums.reduce(function(acc,currval){
+//     console.log(`acc: ${acc} and currval: ${currval}`);
+//     return acc+currval
+// },0)
+
+const myTotal=nums.reduce((acc,curval)=>acc+curval,0)
+
+
+console.log(myTotal);
+
+const shoppingcart=[
+    {
+        itemname:"js",
+        price:2999
+    },
+    {
+        itemname:"java",
+        price:3999
+    },
+    {
+        itemname:".net",
+        price:3500
+    }
+
+]
+
+const price=shoppingcart.reduce((acc,item)=>acc+item.price,0)
+console.log(price);
